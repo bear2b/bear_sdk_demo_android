5a2eaec2716954051b449a68bd7263a67df56c45
Thomas Deffains <thomas@bear2b.com>
2019-07-05 17:15:11 +0200

    BEAR SDK v2.0.12

--- app/src/java/java/com/bear2b/sampleapp/FCMService.java
+++ app/src/java/java/com/bear2b/sampleapp/FCMService.java
@@ -2,10 +2,19 @@ package com.bear2b.sampleapp;
 
 import android.util.Log;
 
+import com.bear.common.sdk.BearSdk;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class FCMService extends FirebaseMessagingService {
+    @Override
+    public void onNewToken(String token) {
+        super.onNewToken(token);
+        if (token != null) {
+            BearSdk.getInstance(this).registerFirebaseToken(token);
+        }
+    }
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.e("DEV", "FCMService onMessageReceived: " + remoteMessage.toString());
--- app/src/kotlin/java/com/bear2b/sampleapp/FCMService.kt
+++ app/src/kotlin/java/com/bear2b/sampleapp/FCMService.kt
@@ -1,10 +1,15 @@
 package com.bear2b.sampleapp
 
 import android.util.Log
+import com.bear.common.sdk.BearSdk
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class FCMService : FirebaseMessagingService() {
+    override fun onNewToken(token: String?) {
+        super.onNewToken(token)
+        token?.let { BearSdk.getInstance(this).registerFirebaseToken(it) }
+    }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Log.e("DEV", "FCMService onMessageReceived: $remoteMessage")
